d6281bfd3dd9967c73a335127c678f3fd122415d
PC <PC@DESKTOP-7GSG7N6>
2025-05-31 22:34:02 -0700

    committed for git new pass by ukasha

--- SpringDemo/src/main/java/com/example/demo/SpringDemoApplication.java
+++ SpringDemo/src/main/java/com/example/demo/SpringDemoApplication.java
@@ -10,12 +10,14 @@ public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
+		String gitPass = "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";
 		System.out.println("added code by TL");
 		System.out.println("added code by DEV");
 		System.out.println("Added by TL on "+new Date());
 		System.out.println("getting exception>>>");
 		System.out.println("added for when DEV1 import the project");
 		System.out.println("Hi i have imported the project");
+		System.out.println("new git password>>>"+gitPass);
 		}
 
 }
